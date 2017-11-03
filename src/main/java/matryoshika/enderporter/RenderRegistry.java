5b5368e06be721429a1913e3603c0c42ca774293
Unknown <mad.matryoshika@gmail.com>
2017-11-03 19:44:03 +0100

    Fix Forge trying to load Client-Side only class

--- src/main/java/matryoshika/enderporter/RenderRegistry.java
+++ src/main/java/matryoshika/enderporter/RenderRegistry.java
@@ -1,30 +1,26 @@
 package matryoshika.enderporter;
 
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.util.math.RayTraceResult;
-import net.minecraftforge.client.event.DrawBlockHighlightEvent;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-@SideOnly(Side.CLIENT)
-@EventBusSubscriber(modid = EnderPorter.MODID)
+@EventBusSubscriber
 public class RenderRegistry {
 	
+	@SideOnly(Side.CLIENT)
 	@SubscribeEvent
-	public static void registerBlockRenderer(ModelRegistryEvent event) {
-		ClientRegistry.bindTileEntitySpecialRenderer(TileEnderPorter.class, new EnderPorterTESR());
+	public static void registerBlockRenderer(net.minecraftforge.client.event.ModelRegistryEvent event) {
+		net.minecraftforge.fml.client.registry.ClientRegistry.bindTileEntitySpecialRenderer(TileEnderPorter.class, new EnderPorterTESR());
 		
-		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(Registry.ENDER_PORTER), 0, new ModelResourceLocation(Registry.ENDER_PORTER.getRegistryName(), "inventory"));
+		net.minecraftforge.client.model.ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(Registry.ENDER_PORTER), 0, new net.minecraft.client.renderer.block.model.ModelResourceLocation(Registry.ENDER_PORTER.getRegistryName(), "inventory"));
 	}
 	
+	@SideOnly(Side.CLIENT)
 	@SubscribeEvent
-	public static void highlight(DrawBlockHighlightEvent event) {
+	public static void highlight(net.minecraftforge.client.event.DrawBlockHighlightEvent event) {
 		if(event.getTarget().typeOfHit == RayTraceResult.Type.BLOCK) {
 			if(event.getPlayer().world.getBlockState(event.getTarget().getBlockPos()).getBlock() == Registry.ENDER_PORTER )
 				event.setCanceled(true);
